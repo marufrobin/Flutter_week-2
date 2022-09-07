fdfc63445ca25f510608d14f10f1307e7ff28d84
Maruf Robin <maruf.robin@northsouth.edu>
2022-09-07 18:44:19 +0600

    Icon added and modifie the DiceApp page.

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -17,9 +17,34 @@ class MyApp extends StatelessWidget {
             'Dice',
           ),
         ),
-        body: Center(
-          child: Image(
-            image: AssetImage('image/dice2.png'),
+        body: DiceApp(),
+      ),
+    );
+  }
+}
+
+class DiceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {  },
+                  child: Image.asset("image/dice1.png"),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: Image.asset("image/dice2.png"),
+              ),
+            ],
           ),
         ),
       ),

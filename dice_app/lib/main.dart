d7c20346d625776afa40fac98582c0694e59be19
Maruf Robin <maruf.robin@northsouth.edu>
2022-09-07 11:44:51 +0600

    Intialization of the dice app and adding the project image.

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -9,11 +9,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.red,
         appBar: AppBar(
           backgroundColor: Colors.red,
           centerTitle: true,
           title: Text(
             'Dice',
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('image/dice2.png'),
           ),
         ),
       ),
